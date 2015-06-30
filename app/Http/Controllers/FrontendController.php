c1910977d846dbd2638b6cc8aafc81e10d990426
vagrant <gerhard@gotarot.com.hk>
2015-06-30 15:20:08 +0000

    added auto-detection of languages

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -14,23 +14,8 @@ class FrontendController extends Controller
 
     public function __construct ()
     {
-        // language settings
-
-
-
-        if (\Cookie::has('lang')) {
-            \App::setLocale(\Cookie::get('lang'));
-        }
-
-        if (
-            (\Input::get('lang') == 'de') ||
-            (\Input::get('lang') == 'en') ||
-            (\Input::get('lang') == 'hk') ||
-            (\Input::get('lang') == 'tw')
-        ) {
-            \Cookie::queue('lang', \Input::get('lang'), 60*24*365);
-            \App::setLocale(\Input::get('lang'));
-        }
+        $language = $this->getLanguage();
+        \App::setLocale($language);
     }
 
     /**
@@ -108,5 +93,42 @@ class FrontendController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function getLanguage()
+    {
+        // language settings from cookie
+        if (\Cookie::has('lang')) {
+            $language = \Cookie::get('lang');
+        }
+
+        // language settings from selection
+        if (
+            (\Input::get('lang') == 'de') ||
+            (\Input::get('lang') == 'en') ||
+            (\Input::get('lang') == 'hk') ||
+            (\Input::get('lang') == 'tw')
+        ) {
+            \Cookie::queue('lang', \Input::get('lang'), 60*24*365);
+            $language = \Input::get('lang');
+        }
+
+        // language settings if neither cookies nor selection
+        if (!isset($language)) {
+            $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+            switch ($lang) {
+                case "zh":
+                    $language = "hk";
+                    break;
+                case "de":
+                    $language = "de";
+                    break;
+                default:
+                    $language = "en";
+                    break;
+            }
+        }
+
+        return $language;
     }
 }
