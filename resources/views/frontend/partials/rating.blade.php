eacbb54574d3662c24afab4ce817040e98d6830a
vagrant <gerhard@gotarot.com.hk>
2015-07-09 13:40:50 +0000

    added rating frontend JS

---
+++ resources/views/frontend/partials/rating.blade.php
@@ -0,0 +1,10 @@
+<div id="rating-wrapper" style="float:left; color: orange;">
+    <div id="rating-wrapper-fix" style="float:left"></div>
+    <div id="rating-wrapper-hover" style="display: none; float:left">
+        <div id="rating1" style="float:left">☆</div>
+        <div id="rating2" style="float:left">★</div>
+        <div id="rating3" style="float:left">★</div>
+        <div id="rating4" style="float:left">★</div>
+        <div id="rating5" style="float:left">★</div>
+    </div>
+</div>
--- resources/views/frontend/supplierProfile.blade.php
+++ resources/views/frontend/supplierProfile.blade.php
@@ -1,13 +1,9 @@
 <?php
-
 	$title = 'This is '.$supplier['officialname'];
-
 	if ($supplier['officialname'] == ''){
 		$title = 'This is '.$supplier['username'];
 	}
-
 ?>
-
 
 @extends('frontend/default')
 
@@ -15,11 +11,112 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="well text-center">
-				{{$title}}
+				<div style="float: left">
+                    {{$title}}
+                </div>
+                @include('frontend/partials/rating')
 			</div>
 		</div>
 		<div class="row">
 			{!!$supplier['profile']!!}
 		</div>
 	</div>
+@stop
+
+@section('js-additions')
+<script>
+    $(document).ready(function(){
+
+        @if (\Input::has('rating') == 'true')
+            var startRating = {{\Input::get('rating')}};
+        @else
+            var startRating = 5;
+        @endif
+
+        @if (\Input::get('ratingactive') == 'true')
+            var activated = true;
+        @endif
+
+        // fill fix div
+        for (var i = 1; i < 6; i++) {
+            if(startRating < i) {
+                $('#rating-wrapper-fix').prepend('☆');
+            } else {
+                $('#rating-wrapper-fix').prepend('★');
+            }
+        }
+
+        if (activated === true) {
+            $('#rating-wrapper-fix').on('mouseenter', function(){
+                $('#rating-wrapper-fix').hide();
+                $('#rating-wrapper-hover').show();
+                $('#rating-wrapper').css('cursor', 'pointer');
+            });
+        }
+
+        $('#rating-wrapper-hover').on('mouseout', function(){
+            $('#rating-wrapper-hover').hide();
+            $('#rating-wrapper-fix').show();
+        });
+
+        $('#rating-wrapper').on('click', function() {
+            $(this).addClass('animated flash');
+        });
+
+        $('#rating1').on('mouseover', function() {
+            $('#rating1').html('★');
+            $('#rating2').html('★');
+            $('#rating3').html('★');
+            $('#rating4').html('★');
+            $('#rating5').html('★');
+        });
+        $('#rating1').on('click', function() {
+            $('#rating-wrapper').html('★★★★★');
+        });
+
+        $('#rating2').on('mouseover', function() {
+            $('#rating1').html('☆');
+            $('#rating2').html('★');
+            $('#rating3').html('★');
+            $('#rating4').html('★');
+            $('#rating5').html('★');
+        });
+        $('#rating2').on('click', function() {
+            $('#rating-wrapper').html('☆★★★★');
+        });
+
+        $('#rating3').on('mouseover', function() {
+            $('#rating1').html('☆');
+            $('#rating2').html('☆');
+            $('#rating3').html('★');
+            $('#rating4').html('★');
+            $('#rating5').html('★');
+        });
+        $('#rating3').on('click', function() {
+            $('#rating-wrapper').html('☆☆★★★');
+        });
+
+        $('#rating4').on('mouseover', function() {
+            $('#rating1').html('☆');
+            $('#rating2').html('☆');
+            $('#rating3').html('☆');
+            $('#rating4').html('★');
+            $('#rating5').html('★');
+        });
+        $('#rating4').on('click', function() {
+            $('#rating-wrapper').html('☆☆☆★★');
+        });
+
+        $('#rating5').on('mouseover', function() {
+            $('#rating1').html('☆');
+            $('#rating2').html('☆');
+            $('#rating3').html('☆');
+            $('#rating4').html('☆');
+            $('#rating5').html('★');
+        });
+        $('#rating5').on('click', function() {
+            $('#rating-wrapper').html('☆☆☆☆★');
+        });
+    });
+</script>
 @stop
