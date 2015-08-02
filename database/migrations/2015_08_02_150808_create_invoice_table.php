bd2307fb88e186847028a860c267ea5c8d591fd0
Gerhard Kuschnik <gerhard@gotarot.com.hk>
2015-08-03 07:14:36 +0800

    fixes

--- database/migrations/2015_08_02_150808_create_invoice_table.php
+++ database/migrations/2015_08_02_150808_create_invoice_table.php
@@ -17,7 +17,8 @@ class CreateInvoiceTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->index();    // who gets invoiced?
             $table->mediumInteger('amount');        // amount in cents
-            $table->smallInteger('rate');        // amount in cents
+            $table->string('supplier');             //
+            $table->smallInteger('rate');           // amount in currency
             $table->enum('currency', ['USD', 'HKD', 'AUD', 'EUR', 'GBP', 'TWD', 'CNY', 'MOP']);
             $table->string('theme')->index();        // gotarot, whitelabel...
             $table->timestamps();
